a0fa7ef57b2e9e571e2e1ec24d80a1468a089337
최유연 <164215451+yuyeon48@users.noreply.github.com>
2026-02-02 14:12:04 +0900

    [Bronze I] Title: 평균, Time: 188 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -9,14 +9,9 @@ public class Main {
         double sum = 0;
         int max = 0;
 
-        // 과목 개수 입력
-        if (sc.hasNextInt()) {
             N = sc.nextInt();
-        } else {
-            return;
-        }
+        
 
-        // 점수 입력 및 최댓값, 총합 구하기
         for (int i = 0; i < N; i++) {
             score = sc.nextInt();
             if (score > max) {
@@ -25,8 +20,6 @@ public class Main {
             sum += score;
         }
 
-        // 새로운 평균 계산: (총합 / 최댓값 * 100) / 과목수
-        // sum이 double이므로 전체 연산은 실수로 진행됩니다.
         System.out.print(sum / max * 100 / N);
 
         sc.close();
